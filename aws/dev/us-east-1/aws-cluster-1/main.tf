3ed6e6bab80b6e7e58b2973b0903f63490b0cea3
Mark James <mark.james@airwalkconsulting.com>
2021-07-20 17:38:01 +0100

    .

--- aws/dev/us-east-1/aws-cluster-1/main.tf
+++ aws/dev/us-east-1/aws-cluster-1/main.tf
@@ -1,73 +1,161 @@
-## https://github.com/k-mitevski/terraform-gke
-
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.42.0"
-    }
-  }
+  required_version = ">= 0.12.0"
 }
 
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
+provider "aws" {
+  version = "~> 3.4"
+  region  = var.region
 }
 
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
+provider "random" {
+  version = "~> 2.3"
 }
 
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  version      = "~> 2.5"
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
+provider "local" {
+  version = "~> 1.4"
+}
 
-  subnets = [
-    {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
-    },
-  ]
+provider "null" {
+  version = "~> 2.1"
+}
 
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
-      {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
-      },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.13"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  #cluster_name = "test-eks-${random_string.suffix.result}"
+  cluster_name = "aws-cluster-1"
+  cluster_version = "1.19"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.1.0.0/8",
     ]
   }
 }
 
-module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  node_pools = [
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.1.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.1.0.0/8",
+      "172.16.1.0/12",
+      "192.168.1.0/16",
+    ]
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
+
+  name                 = "vpc-aws-cluster-1"
+  cidr                 = "10.1.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets       = ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.19"
+  subnets         = module.vpc.private_subnets
+
+  cluster_encryption_config = [
     {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = 1
-      max_count                 = 2
-      disk_size_gb              = 30
+      provider_key_arn = aws_kms_key.eks.arn        # arn:aws:kms:eu-west-2:544294979223:key/9f1bd709-ba1b-40ae-a04e-d3ff4850e88d
+      resources        = ["secrets"]
+    }
+  ]
+
+  #tags = {
+  #  GithubRepo  = "terraform-aws-eks"
+  #  Environment = local.environment
+  #  GithubOrg   = "terraform-aws-modules"
+  #}
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo Mark James"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
   ]
 }
