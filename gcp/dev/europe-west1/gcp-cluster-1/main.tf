19ecbfef1fb95fb3f90ee9ecc38579e0bf1cba6e
Mark James <mark.james@airwalkconsulting.com>
2021-07-20 15:58:58 +0100

    .

--- gcp/dev/europe-west1/gcp-cluster-1/main.tf
+++ gcp/dev/europe-west1/gcp-cluster-1/main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 }
 
-resource "random" "id" {
+resource "random_id" "id" {
 	  byte_length = 8
 }
 
@@ -30,18 +30,18 @@ module "gcp-network" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 2.5"
   project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}-${random.id.hex}"
+  network_name = "${var.network}-${var.env_name}-${random_id.id.hex}"
 
   subnets = [
     {
-      subnet_name   = "${var.subnetwork}-${var.env_name}-${random.id.hex}"
+      subnet_name   = "${var.subnetwork}-${var.env_name}-${random_id.id.hex}"
       subnet_ip     = "10.10.0.0/16"
       subnet_region = var.region
     },
   ]
 
   secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}-${random.id.hex}" = [
+    "${var.subnetwork}-${var.env_name}-${random_id.id.hex}" = [
       {
         range_name    = var.ip_range_pods_name
         ip_cidr_range = "10.20.0.0/16"
